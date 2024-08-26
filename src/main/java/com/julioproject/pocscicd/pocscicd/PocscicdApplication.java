ec81dadc30e85d209e414fc8dac2958151f2a778
Julio <julio.mech@hotmail.com>
2024-08-26 10:49:56 -0300

    First commit

---
+++ src/main/java/com/julioproject/pocscicd/pocscicd/PocscicdApplication.java
@@ -0,0 +1,13 @@
+package com.julioproject.pocscicd.pocscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocscicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocscicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julioproject/pocscicd/pocscicd/PocscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioproject.pocscicd.pocscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocscicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
