df5eb624cd712709c8cf0afdac7cf049cd73d16e
Julio <julio.mech@hotmail.com>
2024-08-27 18:43:46 -0300

    add terrform to pipeline v1

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  region = var.aws_region
 
 }
 
-resource "aws_ecr_repository" "apiOlaImbecil" {
-  name = "apiOlaImbecil"
+resource "aws_ecr_repository" "apiola" {
+  name = "apiola"
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,1 @@
-variable "aws_access_key" {
-  description = ""
-  type        = string
-}
 
-variable "aws_secret_key" {
-  description = ""
-  type        = string
-}
-
-variable "aws_region" {
-  description = ""
-  type        = string
-  default     = "us-east-1"
-}
