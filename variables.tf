393d2f3b50770dd2ee4ca8b8ab2cf2b9bdef3c74
Julio <julio.mech@hotmail.com>
2024-08-27 15:19:28 -0300

    add terraform chekout

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  description = ""
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = ""
+  type        = string
+}
+
+variable "aws_region" {
+  description = ""
+  type        = string
+  default     = "us-east-1"
+}
